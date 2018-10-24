8eb817634f51b380bdd6efbd54cb1489823022a1
nimbus117 <mail@nimbus117.co.uk>
2018-10-24 16:07:40 +0100

    added get all the whisky from a particular distillery that's a specific age

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -1,4 +1,36 @@
 package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.awt.geom.RectangularShape;
+import java.util.List;
+
 public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Distillery> getDistilleryByRegion(String region) {
+        List<Distillery> results = null;
+        Session session = entityManager.unwrap(Session.class);
+
+        try {
+            Criteria cr = session.createCriteria(Distillery.class);
+            cr.add(Restrictions.eq("region", region));
+            results = cr.list();
+        } catch(HibernateException ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return results;
+    }
 }
